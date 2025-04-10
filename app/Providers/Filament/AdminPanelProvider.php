f50a59c02bfbe027640d01242f79f64ccab5a252
PT Spekta Karya Raya <spektakaryaraya@gmail.com>
2025-04-10 08:28:18 +0700

    feat: conditionally display api tokens in admin panel
    This commit introduces a conditional display of the `API Tokens` menu item within the Filament admin panel.
    
    The API Tokens menu item is now only displayed if the `Features::hasApiFeatures()` method
    returns true. This ensures that the menu item is only visible when API features are enabled within the Jetstream configuration.
    
    The Jetstream config has API features commented out, so this commit ensures that menu item for API tokens will not be displayed.
    
    This change improves the user experience by only showing relevant menu items based on the application's configuration.

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -28,6 +28,7 @@ use Illuminate\Session\Middleware\StartSession;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\View\ComponentAttributeBag;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
+use Laravel\Jetstream\Features;
 
 class AdminPanelProvider extends PanelProvider
 {
@@ -90,7 +91,7 @@ class AdminPanelProvider extends PanelProvider
                 \ShuvroRoy\FilamentSpatieLaravelBackup\FilamentSpatieLaravelBackupPlugin::make()
                     ->usingPage(Backups::class),
             ])
-            ->userMenuItems([
+            ->userMenuItems(array_filter([
                 MenuItem::make()
                     ->label(fn () => __('navigation-menu.menu.home'))
                     ->icon('heroicon-o-home')
@@ -99,11 +100,11 @@ class AdminPanelProvider extends PanelProvider
                     ->label(fn () => __('navigation-menu.menu.profile'))
                     ->icon('heroicon-o-user')
                     ->url(fn () => route('profile.show')),
-                MenuItem::make()
+                Features::hasApiFeatures() ? MenuItem::make()
                     ->label(fn () => __('navigation-menu.menu.api_tokens'))
                     ->icon('heroicon-o-key')
-                    ->url(fn () => route('api-tokens.index')),
-            ])
+                    ->url(fn () => route('api-tokens.index')) : null,
+            ]))
             ->renderHook(PanelsRenderHook::SCRIPTS_AFTER, fn () => Blade::render(<<<'BLADE'
             @vite('resources/ts/app.ts')
             BLADE))
