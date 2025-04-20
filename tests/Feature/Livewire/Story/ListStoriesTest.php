731650b897eef48884b2cdf474df02e8ae2dbec2
PT Spekta Karya Raya <spektakaryaraya@gmail.com>
2025-04-20 15:18:41 +0700

    test: add list stories livewire component test
    This commit introduces a new feature: a Livewire component test for listing stories.
    
    The test suite covers:
    
    - Rendering the component successfully.
    - Displaying stories in a table format.
    - Verifying the presence of story titles and creator names within the table.
    
    This addition ensures the `ListStories` Livewire component functions as expected and provides a foundation for future enhancements and modifications.

---
+++ tests/Feature/Livewire/Story/ListStoriesTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Feature\Livewire\Story;
+
+use App\Livewire\Story\ListStories;
+use App\Models\Story;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Livewire\Livewire;
+use Tests\TestCase;
+
+class ListStoriesTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_component_can_be_rendered(): void
+    {
+        Livewire::test(ListStories::class)
+            ->assertStatus(200);
+    }
+
+    public function test_component_renders_and_contains_stories_in_table(): void
+    {
+        /** @var Story[] */
+        $stories = Story::factory()
+            ->count(5)
+            ->ensurePublished()
+            ->create();
+
+        $component = Livewire::test(ListStories::class);
+        $component->assertSuccessful();
+        $component->assertSeeHtml('class="fi-ta"');
+
+        foreach ($stories as $story) {
+            $component->assertSee($story->title);
+            $component->assertSee($story->creator->name);
+        }
+    }
+}
