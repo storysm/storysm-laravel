057cd6f358ae443323c800b92bbe28d2c484f9be
PT Spekta Karya Raya <spektakaryaraya@gmail.com>
2025-05-24 17:37:59 +0700

    refactor: improve story comment card layout
    This commit refactors the story comment card layout to improve its visual appearance and readability.
    
    The changes include:
    
    - Switching from a vertical flex layout to a horizontal flex layout for the main container, aligning the avatar with the comment content.
    - Adding a nested flex container to group the creator information and comment body.
    - Minor adjustments to spacing and alignment to create a cleaner and more structured design.

--- resources/views/livewire/story-comment/story-comment-card.blade.php
+++ resources/views/livewire/story-comment/story-comment-card.blade.php
@@ -1,10 +1,10 @@
 <div>
-    <div class="flex flex-col gap-8">
-        <div class="flex flex-col gap-4">
-            <div class="flex flex-row items-center justify-between gap-2 text-sm">
-                <div class="flex flex-row gap-4">
-                    {{-- Avatar --}}
-                    <x-filament-panels::avatar.user :user="$storyComment->creator" />
+    <div class="flex flex-row items-start justify-between gap-2 text-sm">
+        <div class="flex flex-row gap-4">
+            {{-- Avatar --}}
+            <x-filament-panels::avatar.user :user="$storyComment->creator" />
+            <div class="flex flex-col gap-4">
+                <div class="flex flex-col gap-1">
                     {{-- Creator --}}
                     <div class="flex flex-row gap-1">
                         <p class="font-extrabold">{{ $storyComment->creator?->name }}</p>
@@ -13,43 +13,43 @@
                             {{ $storyComment->created_at->diffForHumans() }}
                         </p>
                     </div>
+
+                    {{-- Body --}}
+                    <p class="max-w-full prose dark:prose-invert">
+                        {{ $storyComment->body }}
+                    </p>
                 </div>
 
-                {{-- Actions --}}
-                @if ($showActions)
-                    @php
-                        $actions = [];
-                        if ($this->editActionPermitted()) {
-                            $actions[] = $this->editAction;
-                        }
-                        if ($this->deleteActionPermitted()) {
-                            $actions[] = $this->deleteAction;
-                        }
-                    @endphp
-
-                    @if (!empty($actions))
-                        <div>
-                            <x-filament-actions::group :actions="$actions" />
-                            <x-filament-actions::modals />
-                        </div>
-                    @endif
+                @if ($showReplyButton)
+                    <div>
+                        {{-- Replies --}}
+                        <x-filament::button :href="route('story-comments.show', $storyComment)" icon="heroicon-m-arrow-uturn-left" :outlined="!$hasUserReplied"
+                            size="xs" tag="a">
+                            {{ $storyComment->formattedReplyCount() }}
+                        </x-filament::button>
+                    </div>
                 @endif
             </div>
-
-            {{-- Body --}}
-            <p class="max-w-full prose dark:prose-invert">
-                {{ $storyComment->body }}
-            </p>
         </div>
 
-        @if ($showReplyButton)
-            <div>
-                {{-- Replies --}}
-                <x-filament::button :href="route('story-comments.show', $storyComment)" icon="heroicon-m-arrow-uturn-left" :outlined="!$hasUserReplied"
-                    size="xs" tag="a">
-                    {{ $storyComment->formattedReplyCount() }}
-                </x-filament::button>
-            </div>
+        {{-- Actions --}}
+        @if ($showActions)
+            @php
+                $actions = [];
+                if ($this->editActionPermitted()) {
+                    $actions[] = $this->editAction;
+                }
+                if ($this->deleteActionPermitted()) {
+                    $actions[] = $this->deleteAction;
+                }
+            @endphp
+
+            @if (!empty($actions))
+                <div>
+                    <x-filament-actions::group :actions="$actions" />
+                    <x-filament-actions::modals />
+                </div>
+            @endif
         @endif
     </div>
 </div>
