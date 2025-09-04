2a7f55924c02e36e949c9edd5264ed003c2823ba
PT Spekta Karya Raya <spektakaryaraya@gmail.com>
2025-09-04 15:07:12 +0700

    feat: add factory for Language model
    This commit introduces a new factory for the `Language` Eloquent model. This factory will be used to generate dummy data for the `Language` model, which is essential for testing and development purposes.
    
    The factory, located at `database/factories/LanguageFactory.php`, defines the default state for a `Language` instance, generating a unique language code and a placeholder country name for the language name.
    
    The `HasFactory` trait has been added to the `Language` model to enable the use of this factory. The PHPDoc block has also been updated to include the `factory()` method signature.

--- app/Models/Language.php
+++ app/Models/Language.php
@@ -2,7 +2,9 @@
 
 namespace App\Models;
 
+use Database\Factories\LanguageFactory;
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Carbon;
 
@@ -12,9 +14,14 @@ use Illuminate\Support\Carbon;
  * @property string $name
  * @property Carbon|null $created_at
  * @property Carbon|null $updated_at
+ *
+ * @method static LanguageFactory factory($count = null)
  */
 class Language extends Model
 {
+    /** @use HasFactory<LanguageFactory> */
+    use HasFactory;
+
     use HasUlids;
 
     protected $fillable = ['code', 'name'];
