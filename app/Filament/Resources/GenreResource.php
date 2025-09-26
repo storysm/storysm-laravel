af8f8697e47c772b88cbddf4f7bd06f59a0c1320
PT Spekta Karya Raya <spektakaryaraya@gmail.com>
2025-09-26 10:12:49 +0700

    feat: add genre resource management
    This commit introduces the Filament resource for managing genres.
    
    Created `app/Filament/Resources/GenreResource.php`
    Created `app/Filament/Resources/GenreResource/Pages/CreateGenre.php`
    Created `app/Filament/Resources/GenreResource/Pages/EditGenre.php`
    Created `app/Filament/Resources/GenreResource/Pages/ListGenres.php`

---
+++ app/Filament/Resources/GenreResource.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\GenreResource\Pages;
+use App\Models\Genre;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+
+class GenreResource extends Resource
+{
+    protected static ?string $model = Genre::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListGenres::route('/'),
+            'create' => Pages\CreateGenre::route('/create'),
+            'edit' => Pages\EditGenre::route('/{record}/edit'),
+        ];
+    }
+}
