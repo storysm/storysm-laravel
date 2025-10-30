98a538bf7982e1990fdd494f2b3b3a0b511138c6
PT Spekta Karya Raya <spektakaryaraya@gmail.com>
2025-10-30 09:14:24 +0700

    feat: implement factory and add translation support for AgeRating model
    This commit enhances the `AgeRating` model by integrating Spatie's translation capabilities and adding a corresponding factory for easier testing and seeding. The model now supports translatable `name` and `description` fields.
    
    Changes:
    
    - Modified `app/Models/AgeRating.php`: Added `HasFactory` and `HasTranslations` traits, defined `$translatable`, `$fillable`, and `$casts` properties.
    - Created `database/factories/AgeRatingFactory.php`: Added a new factory to generate mock data for the `AgeRating` model, including localized names and descriptions.

--- app/Models/AgeRating.php
+++ app/Models/AgeRating.php
@@ -3,9 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Support\Carbon;
+use Spatie\Translatable\HasTranslations;
 
 /**
  * @property string $id
@@ -17,7 +19,38 @@ use Illuminate\Support\Carbon;
  */
 class AgeRating extends Model
 {
+    /** @use HasFactory<\Database\Factories\AgeRatingFactory> */
+    use HasFactory;
+    use HasTranslations;
+
     use HasUlids;
+
+    /**
+     * The attributes that are translatable.
+     *
+     * @var array<int, string>
+     */
+    public array $translatable = ['name', 'description'];
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'name',
+        'description',
+        'age_representation',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'age_representation' => 'integer',
+    ];
 
     /**
      * Get the stories associated with the age rating.
