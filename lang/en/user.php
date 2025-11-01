01a6f916f5e5bea0bceb919b1c2fdaed36bbafe6
PT Spekta Karya Raya <spektakaryaraya@gmail.com>
2025-11-01 19:02:15 +0700

    feat: display notice for stories posted on behalf of guest users
    This commit introduces functionality to display a notice to users viewing a story that was posted by a staff member acting as a guest user. This informs readers that the original author may not be able to respond to comments.
    
    Changes:
    
    - Modified `lang/en/user.php`: Added `guest_user_notice` translation key.
    - Modified `lang/id/user.php`: Added `guest_user_notice` translation key in Indonesian.
    - Modified `resources/views/livewire/story/view-story.blade.php`: Conditionally displays an information section containing the guest user notice above the comment section if the story creator has the permission to act as a guest user.

--- lang/en/user.php
+++ lang/en/user.php
@@ -9,5 +9,6 @@ return [
         'user_type_label' => 'User Type',
         'guest_users_label' => 'Guest Users',
         'regular_users_label' => 'Regular Users',
+        'guest_user_notice' => 'This work was posted on behalf of the author by our staff. The author will not be able to respond to comments.',
     ],
 ];
